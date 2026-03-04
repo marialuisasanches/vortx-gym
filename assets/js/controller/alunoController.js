9508da483aef9d5a1a42705f80bb9bb1a4feaaf1
marialuisasanches <marialuisarmsanchess@gmail.com>
2026-03-04 18:12:03 -0300

    feat: início da estruturação do js no padrão mvc

---
+++ assets/js/controller/alunoController.js
@@ -0,0 +1,23 @@
+const aluno = new Aluno(
+  (nome = document.querySelector("#nome" + "#sobrenome")),
+  (datNasc = document.querySelector("#datNasc")),
+  (email = document.querySelector("#email")),
+  (senha = document.querySelector("#senha")),
+  (telefone = document.querySelector("#telefone")),
+  (cpf = document.querySelector("#cpf")),
+  (plano = document.querySelector("#plano")),
+  (modalidade = document.querySelector("modalidade")),
+  (objetivo = document.querySelector("#objetivo")),
+);
+
+console.log(
+  nome,
+  datNasc,
+  email,
+  senha,
+  telefone,
+  cpf,
+  plano,
+  modalidade,
+  objetivo,
+);
---
+++ assets/js/model/aluno.js
@@ -0,0 +1,35 @@
+let id;
+let nome;
+let datNasc;
+let email;
+let senha;
+let telefone;
+let cpf;
+let plano;
+let modalidade;
+let objetivo;
+
+class Aluno {
+  constructor(
+    nome,
+    datNasc,
+    email,
+    senha,
+    telefone,
+    cpf,
+    plano,
+    modalidade,
+    objetivo,
+  ) {
+    this.id = id;
+    this.nome = nome;
+    this.datNasc = datNasc;
+    this.email = email;
+    this.senha = senha;
+    this.telefone = telefone;
+    this.cpf = cpf;
+    this.plano = plano;
+    this.modalidade = modalidade;
+    this.objetivo = objetivo;
+  }
+}
